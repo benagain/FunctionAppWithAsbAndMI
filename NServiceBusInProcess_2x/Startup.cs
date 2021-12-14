625326a8cbd6bc2b4335297a5b77e619f4de6233
Ben Arnold <ben@arnoldfamily.me.uk>
2021-12-14 15:24:31 +0000

    NSB example

---
+++ NServiceBusInProcess_2x/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using NServiceBus;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+[assembly: NServiceBusTriggerFunction(Startup.EndpointName)]
+
+internal class Startup : FunctionsStartup
+{
+    public const string EndpointName = "MyQueue";
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.UseNServiceBus(() => new ServiceBusTriggeredEndpointConfiguration(EndpointName, "AzureWebJobsServiceBus"));
+    }
+}
